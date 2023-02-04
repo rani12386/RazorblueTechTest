f230c1bd1dffa06bc9f8fcce63055584260000cd
meena <meenarani.samala@gmail.com>
2023-02-04 12:28:04 +0000

    Initial Commit On Implementation of Anagram

--- RazorblueTechTest.Anagram/Anagram.cs
+++ RazorblueTechTest.Anagram/Anagram.cs
@@ -2,20 +2,20 @@
 {
     public static class Anagram
     {
-        public static bool IsAnagram(string s1, string s2)
+        public static bool IsAnagram(string str1, string str2)
         {
-            if (s1.Length != s2.Length)
+            if (str1.Length != str2.Length)
                 return false;
-            var s1Array = s1.ToLower().ToCharArray();
-            var s2Array = s2.ToLower().ToCharArray();
+            var s1Array = str1.ToLower().ToCharArray();
+            var s2Array = str2.ToLower().ToCharArray();
 
             Array.Sort(s1Array);
             Array.Sort(s2Array);
 
-            s1 = new string(s1Array);
-            s2 = new string(s2Array);
+            str1 = new string(s1Array);
+            str2 = new string(s2Array);
 
-            return s1 == s2;
+            return str1 == str2;
         }
     }
 }
--- RazorblueTechTest.UnitTest/AnagramTest.cs
+++ RazorblueTechTest.UnitTest/AnagramTest.cs
@@ -6,6 +6,7 @@ namespace RazorblueTechTest.UnitTest
         [InlineData("abc", "cba", true)]
         [InlineData("abcd", "dcba", true)]
         [InlineData("abcd", "abda", false)]
+
         public void IsAnagramTests(string input1, string input2, bool expected)
         {
             Assert.Equal(expected, Anagram.Anagram.IsAnagram(input1, input2));
